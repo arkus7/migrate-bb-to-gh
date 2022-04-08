948a0e9229a9e2c380b9822267077acc22dbf791
Arkadiusz Żmudzin <arek@moodup.team>
2022-04-08 19:47:46 +0200

    Store whole repositories vector in migrate repositories action

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 use anyhow::anyhow;
 use bitbucket::Repository;
 use dialoguer::{theme::ColorfulTheme, FuzzySelect, Input, MultiSelect, Select};
@@ -11,14 +13,15 @@ mod github;
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "snake_case")]
 enum Action {
+    MigrateRepositories {
+        repositories: Vec<Repository>,
+    },
     CreateTeam {
         name: String,
         repositories: Vec<String>,
     },
-    MigrateRepositories {
-        names: Vec<String>,
-    },
     AssignRepositoriesToTeam {
+        team_name: String,
         team_id: u32,
         permission: TeamRepositoryPermission,
         repositories: Vec<String>,
@@ -42,7 +45,7 @@ async fn main() -> Result<(), anyhow::Error> {
 
     let project = projects.get(selection).expect("No project selected");
 
-    spinner.set_message(format!("Fetching repositories from {} project", project));
+    let spinner = create_spinner(format!("Fetching repositories from {} project", project));
     let repositories = bitbucket::get_repositories(project.get_key()).await?;
     spinner.finish_with_message("Fetched!");
 
@@ -51,13 +54,14 @@ async fn main() -> Result<(), anyhow::Error> {
         .items(&repositories)
         .interact()?;
 
-    if selection.len() == 0 {
+    if selection.is_empty() {
         return Err(anyhow!("At least one repository must be selected"));
     }
 
-    let repositories: Vec<&Repository> = selection
+    let repositories: Vec<Repository> = selection
         .into_iter()
         .flat_map(|idx| repositories.get(idx))
+        .cloned()
         .collect::<Vec<_>>();
 
     dbg!(&repositories);
@@ -66,9 +70,8 @@ async fn main() -> Result<(), anyhow::Error> {
         .iter()
         .map(|r| r.full_name.to_owned())
         .collect();
-    let migrate_action = Action::MigrateRepositories {
-        names: repositories_names.clone(),
-    };
+
+    let migrate_action = Action::MigrateRepositories { repositories };
 
     let team_choice = Select::with_theme(&theme)
         .with_prompt("Team settings")
@@ -116,6 +119,7 @@ async fn main() -> Result<(), anyhow::Error> {
             };
 
             Action::AssignRepositoriesToTeam {
+                team_name: team.name.clone(),
                 team_id: team.id,
                 permission,
                 repositories: repositories_names,
@@ -131,7 +135,7 @@ async fn main() -> Result<(), anyhow::Error> {
     Ok(())
 }
 
-fn create_spinner(message: &'static str) -> ProgressBar {
+fn create_spinner<T: Into<Cow<'static, str>>>(message: T) -> ProgressBar {
     let pb = ProgressBar::new_spinner();
     pb.enable_steady_tick(120);
 
