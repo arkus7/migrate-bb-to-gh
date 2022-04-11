9e1197eeda3f8b3b10859d4b0294fa621b0d499f
Arkadiusz Żmudzin <arek@moodup.team>
2022-04-11 22:10:08 +0200

    Finish contextx and building app in circleci wizard

--- src/circleci.rs
+++ src/circleci.rs
@@ -1,8 +1,10 @@
 pub(crate) mod wizard {
-    use std::{path::PathBuf, str::FromStr, collections::HashSet};
+    use std::{any, collections::HashSet, fs::File, hash::Hash, path::PathBuf, str::FromStr};
 
     use anyhow::{anyhow, Ok};
-    use dialoguer::{theme::ColorfulTheme, Confirm, FuzzySelect, MultiSelect};
+    use dialoguer::{
+        theme::ColorfulTheme, Confirm, FuzzySelect, Input, MultiSelect, Password, Select,
+    };
 
     use crate::{
         circleci::migrate::Action,
@@ -46,9 +48,16 @@ pub(crate) mod wizard {
                     actions.push(move_envs_action);
                 }
 
-                let create_contexts_actions = self.create_contexts_actions(&repository, &config).await?;
+                let create_contexts_actions =
+                    self.create_contexts_actions(&config, &actions).await?;
+                actions.extend(create_contexts_actions);
+
+                if let Some(start_build_action) = self.start_build(&repository).await? {
+                    actions.push(start_build_action);
+                }
             }
-            dbg!(&actions);
+
+            self.save_migration_file(&actions)?;
 
             Ok(())
         }
@@ -58,7 +67,7 @@ pub(crate) mod wizard {
                 "Fetching {} environment variables",
                 &repository.name
             ));
-            let env_vars: Vec<_> = api::get_env_vars(&repository.full_name)
+            let env_vars: Vec<_> = api::get_env_vars(api::Vcs::Bitbucket, &repository.full_name)
                 .await?
                 .into_iter()
                 .map(|e| e.name)
@@ -176,34 +185,195 @@ pub(crate) mod wizard {
             }
         }
 
-        async fn create_contexts_actions(&self, repository: &Repository, config: &Config) -> anyhow::Result<Vec<Action>> {
-          if config.contexts.is_empty() {
-            return Ok(vec![]);
-          }
+        async fn create_contexts_actions(
+            &self,
+            config: &Config,
+            actions: &[Action],
+        ) -> anyhow::Result<Vec<Action>> {
+            if config.contexts.is_empty() {
+                return Ok(vec![]);
+            }
 
-          println!("Found {} contexts", config.contexts.len());
-          for context in &config.contexts {
-            println!(" - {}", context);
-          }
+            println!(
+                "Found {} contexts in .circleci/config.yml file",
+                config.contexts.len()
+            );
+            for context in &config.contexts {
+                println!(" - {}", context);
+            }
 
-          let spinner = spinner::create_spinner("Fetching contexts from CircleCI...");
-          let exisiting_contexts = api::get_contexts().await?;
-          spinner.finish_with_message(format!("Found {} contexts", exisiting_contexts.len()));
+            let spinner = spinner::create_spinner("Fetching GitHub contexts from CircleCI...");
+            let exisiting_contexts = api::get_contexts(api::Vcs::GitHub).await?;
+            spinner.finish_with_message(format!(
+                "Found {} contexts defined in GitHub org",
+                exisiting_contexts.len()
+            ));
 
-          let existing_names = exisiting_contexts.iter().map(|context| context.name.clone()).collect::<HashSet<_>>();
-          let diff = config.contexts.difference(&existing_names).cloned().collect::<Vec<_>>();
+            let existing_names = exisiting_contexts
+                .iter()
+                .map(|context| context.name.clone())
+                .collect::<HashSet<_>>();
 
-          if diff.is_empty() {
-            println!("All contexts already exist, skipping...");
-            return Ok(vec![]);
-          }
+            let diff = config
+                .contexts
+                .difference(&existing_names)
+                .cloned()
+                .collect::<HashSet<_>>();
 
-          println!("Found {} new contexts: {}", diff.len(), diff.join(", "));
+            let migrated_contexts = actions
+                .iter()
+                .filter(|action| match action {
+                    Action::CreateContext { .. } => true,
+                    _ => false,
+                })
+                .map(|action| match action {
+                    Action::CreateContext { name, .. } => name.clone(),
+                    _ => unreachable!(),
+                })
+                .collect::<HashSet<_>>();
 
-          // TODO: check which contexts are already on CircleCI
-          // TODO: select contexts to create
-          // TODO: select variables to fill, ask for each one value
-          todo!("creating contexts not yet implemented");
+            let diff = diff
+                .difference(&migrated_contexts)
+                .cloned()
+                .collect::<Vec<_>>();
+
+            if diff.is_empty() {
+                println!("All contexts already exist, skipping...");
+                return Ok(vec![]);
+            }
+
+            println!("Found {} new contexts: {}", diff.len(), diff.join(", "));
+
+            let context_selection = MultiSelect::with_theme(&self.theme)
+                .with_prompt("Select contexts to create")
+                .items(&diff)
+                .interact()?;
+
+            if context_selection.is_empty() {
+                println!("No contexts selected, skipping...");
+                return Ok(vec![]);
+            }
+
+            let contexts: Vec<String> = context_selection
+                .into_iter()
+                .flat_map(|idx| diff.get(idx))
+                .cloned()
+                .collect::<Vec<_>>();
+
+            let input_variables_values = Confirm::with_theme(&self.theme)
+                .with_prompt("Do you want to input variable values to the new contexts? (No = creating empty contexts)")
+                .interact()?;
+
+            if !input_variables_values {
+                println!("Creating empty contexts...");
+                return Ok(contexts
+                    .into_iter()
+                    .map(|context| Action::CreateContext {
+                        name: context,
+                        variables: vec![],
+                    })
+                    .collect());
+            }
+
+            let spinner = spinner::create_spinner("Fetching Bitbucket contexts from CircleCI...");
+            let exisiting_contexts = api::get_contexts(api::Vcs::Bitbucket).await?;
+            spinner.finish_with_message(format!(
+                "Found {} contexts in Bitbucket",
+                exisiting_contexts.len()
+            ));
+
+            let mut actions: Vec<Action> = vec![];
+
+            for context in contexts {
+                if let Some(bb_context) = exisiting_contexts.iter().find(|c| c.name == context) {
+                    let spinner =
+                        spinner::create_spinner(format!("Fetching {} context variables", &context));
+                    let variables = api::get_context_variables(&bb_context.id).await?;
+                    spinner.finish_with_message(format!(
+                        "Found {} variables for '{}' context",
+                        variables.len(),
+                        &context
+                    ));
+
+                    let variables = variables
+                        .into_iter()
+                        .map(|variable| {
+                            let variable = variable.variable;
+                            let value = Input::with_theme(&self.theme)
+                                .with_prompt(format!("Input value for '{}' variable:", variable))
+                                .interact()
+                                .expect("invalid input for variable value");
+                            (variable, value)
+                        })
+                        .collect::<Vec<_>>();
+                    actions.push(Action::CreateContext {
+                        name: context,
+                        variables,
+                    });
+                } else {
+                    println!(
+                        "Context {} not found in Bitbucket, adding empty context",
+                        context
+                    );
+                    actions.push(Action::CreateContext {
+                        name: context,
+                        variables: vec![],
+                    });
+                }
+            }
+            Ok(actions)
+        }
+
+        async fn start_build(&self, repo: &Repository) -> anyhow::Result<Option<Action>> {
+            let confirm = Confirm::with_theme(&self.theme)
+                .with_prompt(format!(
+                    "Do you want to start a build for {} repository on CircleCI?",
+                    &repo.name
+                ))
+                .interact()?;
+
+            if !confirm {
+                return Ok(None);
+            }
+
+            let use_default_branch = Confirm::with_theme(&self.theme)
+                .with_prompt(format!(
+                    "Do you want to use default branch for build ({})?",
+                    &repo.default_branch
+                ))
+                .interact()?;
+
+            if use_default_branch {
+                return Ok(Some(Action::RunFirstBuild {
+                    repository_name: repo.full_name.clone(),
+                    branch: repo.default_branch.clone(),
+                }));
+            }
+
+            let spinner = spinner::create_spinner("Fetching branches from GitHub...");
+            let branches = github::get_repo_branches(&repo.full_name).await?;
+            spinner.finish_with_message(format!("Found {} branches", branches.len()));
+
+            let branches = branches
+                .into_iter()
+                .map(|branch| branch.name.clone())
+                .collect::<Vec<_>>();
+
+            let default_idx = branches
+                .iter()
+                .position(|branch| branch == &repo.default_branch)
+                .unwrap_or(0);
+
+            let branch_selection = Select::with_theme(&self.theme)
+                .with_prompt("Select branch to build")
+                .items(&branches)
+                .default(default_idx)
+                .interact()?;
+
+            Ok(Some(Action::RunFirstBuild {
+                repository_name: repo.full_name.clone(),
+                branch: branches[branch_selection].clone(),
+            }))
         }
 
         fn parse_config(&self, config: &FileContents) -> anyhow::Result<super::config::Config> {
@@ -212,6 +382,24 @@ pub(crate) mod wizard {
             let config = super::config::Config::from_str(&config)?;
 
             Ok(config)
+        }
+
+        fn save_migration_file(&self, actions: &[Action]) -> Result<(), anyhow::Error> {
+            if self.output.exists() {
+                let overwrite = Confirm::with_theme(&self.theme)
+                    .with_prompt("Migration file already exists. Overwrite?")
+                    .default(false)
+                    .interact()?;
+
+                if !overwrite {
+                    return Err(anyhow!("Migration file already exists"));
+                }
+            }
+            let mut file = File::create(&self.output)?;
+
+            serde_json::to_writer(&mut file, actions)?;
+
+            Ok(())
         }
     }
 }
@@ -223,6 +411,27 @@ mod api {
     const TOKEN: &str = "6b6e68c774603758ab9c526dda94258ddfbdca8f";
     const AUTH_HEADER: &str = "Circle-Token";
 
+    pub enum Vcs {
+        Bitbucket,
+        GitHub,
+    }
+
+    impl Vcs {
+        const fn org_id(&self) -> &str {
+            match self {
+                Vcs::Bitbucket => "0cb7bbc7-b867-455b-a6cb-fa51b56d65af",
+                Vcs::GitHub => "d5d2a07e-1731-435c-8e9f-916b6d9dc197",
+            }
+        }
+
+        const fn slug_prefix(&self) -> &str {
+            match self {
+                Vcs::Bitbucket => "bitbucket",
+                Vcs::GitHub => "gh",
+            }
+        }
+    }
+
     #[derive(Serialize, Deserialize, Debug, Clone)]
     pub struct EnvVar {
         pub name: String,
@@ -230,7 +439,7 @@ mod api {
     }
 
     #[derive(Serialize, Deserialize, Debug, Clone)]
-    pub struct EnvVarsResponse {
+    struct EnvVarsResponse {
         items: Vec<EnvVar>,
         next_page_token: Option<String>,
     }
@@ -242,14 +451,26 @@ mod api {
     }
 
     #[derive(Serialize, Deserialize, Debug, Clone)]
-
-    pub struct ContextsResponse {
-      items: Vec<Context>,
-      next_page_token: Option<String>,
+    struct ContextsResponse {
+        items: Vec<Context>,
+        next_page_token: Option<String>,
     }
 
-    pub async fn get_env_vars(full_repo_name: &str) -> anyhow::Result<Vec<EnvVar>> {
-        let project_slug = format!("bitbucket/{}", full_repo_name);
+    #[derive(Serialize, Deserialize, Debug, Clone)]
+    struct ContextVariablesResponse {
+        items: Vec<ContextVariable>,
+        next_page_token: Option<String>,
+    }
+
+    #[derive(Serialize, Deserialize, Debug, Clone)]
+
+    pub struct ContextVariable {
+        pub variable: String,
+        pub context_id: String,
+    }
+
+    pub async fn get_env_vars(vcs: Vcs, full_repo_name: &str) -> anyhow::Result<Vec<EnvVar>> {
+        let project_slug = format!("{}/{}", vcs.slug_prefix(), full_repo_name);
         let url = format!(
             "https://circleci.com/api/v2/project/{project_slug}/envvar",
             project_slug = project_slug,
@@ -260,11 +481,24 @@ mod api {
         Ok(res.items)
     }
 
-    pub async fn get_contexts() -> anyhow::Result<Vec<Context>> {
-        const GITHUB_ORG_ID: &str = "d5d2a07e-1731-435c-8e9f-916b6d9dc197";
-        let url = format!("https://circleci.com/api/v2/context?owner-id={org_id}", org_id = GITHUB_ORG_ID);
+    pub async fn get_contexts(vcs: Vcs) -> anyhow::Result<Vec<Context>> {
+        let url = format!(
+            "https://circleci.com/api/v2/context?owner-id={org_id}",
+            org_id = vcs.org_id()
+        );
 
         let res: ContextsResponse = send_get_request(url).await?;
+
+        Ok(res.items)
+    }
+
+    pub async fn get_context_variables(context_id: &str) -> anyhow::Result<Vec<ContextVariable>> {
+        let url = format!(
+            "https://circleci.com/api/v2/context/{context_id}/environment-variable",
+            context_id = context_id
+        );
+
+        let res: ContextVariablesResponse = send_get_request(url).await?;
 
         Ok(res.items)
     }
@@ -308,9 +542,14 @@ mod migrate {
 }
 
 mod config {
-    use std::{collections::{HashSet, HashMap}, str::FromStr};
+    use std::{
+        collections::{HashMap, HashSet},
+        str::FromStr,
+    };
 
     use serde::{Deserialize, Serialize};
+
+    use self::raw::Context;
 
     #[derive(Serialize, Deserialize, Debug)]
     pub struct Config {
@@ -332,14 +571,18 @@ mod config {
                 .map(|j| j.into_values())
                 .flatten()
                 .flat_map(|j| j.context)
-                .flatten()
-                .for_each(|c| {
-                    contexts.insert(c);
+                .for_each(|c| match c {
+                    Context::String(ctx) => {
+                        contexts.insert(ctx);
+                    }
+                    Context::Vec(ctx) => {
+                        ctx.into_iter().for_each(|c| {
+                            contexts.insert(c);
+                        });
+                    }
                 });
 
-            Ok(Config {
-                contexts,
-            })
+            Ok(Config { contexts })
         }
     }
 
@@ -360,7 +603,14 @@ mod config {
 
         #[derive(Debug, PartialEq, Serialize, Deserialize)]
         pub(crate) struct Job {
-            pub context: Option<Vec<String>>,
+            pub context: Option<Context>,
+        }
+
+        #[derive(Debug, PartialEq, Serialize, Deserialize)]
+        #[serde(untagged)]
+        pub(crate) enum Context {
+            String(String),
+            Vec(Vec<String>),
         }
     }
 }
