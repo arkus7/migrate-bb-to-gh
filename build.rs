68ab8e0bdfa7309deb7c4146fbf6d6f803684e80
Arkadiusz Żmudzin <arek@moodup.team>
2022-04-26 23:41:26 +0200

    Add build script to encrypt config file

---
+++ build.rs
@@ -0,0 +1,11 @@
+use std::error::Error;
+use std::fs::write;
+use base64;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let config = include_bytes!("config.yml");
+    let encoded = base64::encode(config);
+    write("config.encrypted.yml", encoded)?;
+    println!("cargo:rerun-if-changed=config.yml");
+    Ok(())
+}
