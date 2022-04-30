f473a3cdb85043bfdcbbeb384d2303f842ea4f4d
Arkadiusz Żmudzin <arek@moodup.team>
2022-04-30 21:51:49 +0200

    Make `Confirm` default to false, add function to change behaviour

--- src/prompts/confirm.rs
+++ src/prompts/confirm.rs
@@ -3,13 +3,20 @@ use std::io;
 
 pub struct Confirm {
     prompt: String,
+    default: bool,
 }
 
 impl Confirm {
     pub fn with_prompt<S: Into<String>>(prompt: S) -> Self {
         Self {
             prompt: prompt.into(),
+            default: false,
         }
+    }
+
+    pub fn default(&mut self, default: bool) -> &mut Self {
+        self.default = default;
+        self
     }
 
     pub fn interact(&self) -> io::Result<bool> {
@@ -17,6 +24,7 @@ impl Confirm {
 
         Confirm::with_theme(&default_theme())
             .with_prompt(&self.prompt)
+            .default(self.default)
             .interact()
     }
 }
