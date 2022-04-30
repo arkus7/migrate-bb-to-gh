c636c8313fe46c74ccb34b17973c3dcfa7b4c656
Arkadiusz Żmudzin <arek@moodup.team>
2022-04-30 20:42:00 +0200

    Add Select prompt

--- src/prompts/mod.rs
+++ src/prompts/mod.rs
@@ -4,11 +4,13 @@ mod confirm;
 mod fuzzy_select;
 mod multi_select;
 mod input;
+mod select;
 
 pub use fuzzy_select::FuzzySelect;
 pub use multi_select::MultiSelect;
 pub use confirm::Confirm;
 pub use input::Input;
+pub use select::Select;
 
 fn default_theme() -> ColorfulTheme {
     ColorfulTheme::default()
