530c209f8fe79aef9fbde65cffbf931b373d01bb
Arkadiusz Żmudzin <arek@moodup.team>
2022-05-07 23:40:51 +0200

    Update message when CircleCI wizard couldn't find repository in Bitbucket org

--- src/circleci/wizard.rs
+++ src/circleci/wizard.rs
@@ -147,7 +147,11 @@ impl Wizard {
                 &repository.name
             ));
             let bb_repo = self.bitbucket.get_repository(&repository.full_name).await?;
-            spinner.finish_with_message(format!("Found {:?} repository in Bitbucket", bb_repo));
+            let msg: String = match bb_repo {
+                Some(ref repo) => format!("Found {} repository in Bitbucket, assuming it has no env variables", repo.full_name),
+                None => format!("No repository named '{}' found in Bitbucket!", &repository.name)
+            };
+            spinner.finish_with_message(msg.to_owned());
             if bb_repo.is_none() {
                 let manually_map = Confirm::with_prompt(format!(
                     "No repository named {} found in Bitbucket, do you want to manually map it?",
